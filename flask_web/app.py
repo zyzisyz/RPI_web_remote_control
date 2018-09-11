8740b76053780cb08225812dc9d518b1c97347a6
Rayue <18810368023@163.com>
2018-09-11 19:46:47 +0800

    update

--- flask_web/app.py
+++ flask_web/app.py
@@ -10,7 +10,7 @@ from time import sleep
 
 app = Flask(__name__)
 
-num = 0
+state = 0    #记录空调状态
 temp = [26]
 wet = [12]
 Pi_time = [1]
@@ -31,11 +31,15 @@ def update_data():
         sleep(3)
 
 
-def Judge_on_off(num):
-    if num % 2 == 0:
-        return render_template('index.html', Temperature=temp[-1], Wet=wet[-1], Time=Pi_time[-1], Turn="turn off")
+def Judge_on_off():
+    global state
+    global temp
+    global wet
+    global Pi_time
+    if state > 0:
+        return render_template('index.html', Temperature=temp[-1], Wet=wet[-1], Time=Pi_time[-1], Turn="turn off", State = state)
     else:
-        return render_template('index.html', Temperature=temp[-1], Wet=wet[-1], Time=Pi_time[-1], Turn="turn on")
+        return render_template('index.html', Temperature=temp[-1], Wet=wet[-1], Time=Pi_time[-1], Turn="turn on", State = state)
 
 
 @app.route('/')
@@ -46,79 +50,168 @@ def hello_world():
     return render_template('index.html', Temperature=temp[-1], Wet=wet[-1], Time=Pi_time[-1])
 
 
-@app.route('/index')
-def index():
-    return render_template('index.html', Temperature=temp[-1], Wet=wet[-1], Time=Pi_time[-1])
-
-
 @app.route('/IR_ON')
 def ir_on():
-    global num
-    global temp
-    global wet
-    global Pi_time
-    if num % 2 == 0:
-        print('ir_on')
-        os.system('irsend SEND_ONCE AIR KEY_OPEN')
-        num += 1
-        return render_template('index.html', Temperature=temp[-1], Wet=wet[-1], Time=Pi_time[-1], Turn="turn off")
-    else:
+    global state
+    if state:
         print("ir_off")
         os.system('irsend SEND_ONCE AIR KEY_OFF')
-        num += 1
-        return render_template('index.html', Temperature=temp[-1], Wet=wet[-1], Time=Pi_time[-1], Turn="turn on")
+        state = 0
+    else:
+        print('ir_on')
+        os.system('irsend SEND_ONCE AIR KEY_26')
+        state = 26
+    return Judge_on_off()
 
 
 @app.route('/IR_UP')
 def ir_up():
-    global temp
-    global wet
-    global Pi_time
-    global num
-    print('ir_up')
-    os.system('irsend SEND_ONCE AIR KEY_UP')
-    return Judge_on_off(num)
+    global state
+    switch(state):
+        case 0:
+        case 30:
+            break
+        case 16: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_17')
+            break
+        case 17: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_18')
+            break
+        case 18: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_19')
+            break
+        case 19: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_20')
+            break
+        case 20: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_21')
+            break
+        case 21: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_22')
+            break
+        case 22: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_23')
+            break
+        case 23: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_24')
+            break
+        case 24: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_25')
+            break
+        case 25: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_26')
+            break
+        case 26: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_27')
+            break
+        case 27: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_28')
+            break
+        case 28: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_29')
+            break
+        case 29: 
+            print('ir_up')
+            os.system('irsend SEND_ONCE AIR KEY_30')
+            break   
+    return Judge_on_off()
 
 
 @app.route('/IR_DOWN')
 def ir_DOWN():
-    global temp
-    global wet
-    global Pi_time
-    global num
-    print('ir_down')
-    os.system('irsend SEND_ONCE AIR KEY_DOWN')
-    return Judge_on_off(num)
+    global state
+    switch(state):
+        case 0:
+        case 16:
+            break
+        case 17: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_16')
+            break
+        case 18: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_17')
+            break
+        case 19: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_18')
+            break
+        case 20: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_19')
+            break
+        case 21: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_20')
+            break
+        case 22: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_21')
+            break
+        case 23: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_22')
+            break
+        case 24: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_23')
+            break
+        case 25: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_24')
+            break
+        case 26: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_25')
+            break
+        case 27: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_26')
+            break
+        case 28: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_27')
+            break
+        case 29: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_28')
+            break 
+        case 30: 
+            print('ir_down')
+            os.system('irsend SEND_ONCE AIR KEY_29')
+            break 
+    return Judge_on_off()
 
 
 @app.route('/LE_ON')
 def led_on():
-    global temp
-    global wet
-    global Pi_time
-    global num
     LED_turn_on()
-    return Judge_on_off(num)
+    return Judge_on_off()
 
 
 @app.route('/LED_OFF')
 def led_off():
-    global temp
-    global wet
-    global Pi_time
-    global num
     LED_turn_off()
-    return Judge_on_off(num)
+    return Judge_on_off()
 
 
 @app.route('/LED_SHINE')
 def led_shine():
-    global temp
-    global wet
-    global Pi_time
-    global num
     LED_shine()
-    return Judge_on_off(num)
+    return Judge_on_off()
 
 
 def run_app():
