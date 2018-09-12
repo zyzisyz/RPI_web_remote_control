5e35b334019903bd6099a211d948f7b500a8dc9c
zyz <zyziszy@foxmail.com>
2018-09-12 13:23:37 +0800

    add frp net setting

--- flask_web/app.py
+++ flask_web/app.py
@@ -4,7 +4,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from get_data import web_data
 import os
-from rpi_control import *
+import rpi_control
 import threading
 from time import sleep
 
@@ -28,7 +28,7 @@ def update_data():
         print("temp:", temp[-1])
         print("wet:", wet[-1])
 
-        sleep(3)
+        sleep(5)
 
 
 def Judge_on_off(num):
@@ -97,7 +97,7 @@ def led_on():
     global wet
     global Pi_time
     global num
-    LED_turn_on()
+    rpi_control.LED_shine()
     return Judge_on_off(num)
 
 
@@ -107,7 +107,7 @@ def led_off():
     global wet
     global Pi_time
     global num
-    LED_turn_off()
+    rpi_control.LED_turn_off()
     return Judge_on_off(num)
 
 
@@ -117,7 +117,7 @@ def led_shine():
     global wet
     global Pi_time
     global num
-    LED_shine()
+    rpi_control.LED_shine()
     return Judge_on_off(num)
 
 
--- flask_web/rpi_control.py
+++ flask_web/rpi_control.py
@@ -1,22 +1,56 @@
 #!python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import os
+import time
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(37, GPIO.OUT)
+
+
+def IR_turn_on():
+    pass
+
+
+def IR_turn_off():
+    pass
+
+
+def IR_turn_down():
+    pass
+
+
+def IR_turn_up():
+    pass
 
 
 def LED_turn_on():
-    os.system('irsend SEND_ONCE AIR KEY_OPEN')
+    GPIO.output(37, GPIO.LOW)
+    print("LED turn on")
 
 
 def LED_turn_off():
-    os.system('irsend SEND_ONCE AIR KEY_OFF')
-    
+    GPIO.output(37, GPIO.HIGH)
+    print("LED turn off")
+
 
 def LED_shine():
-    pass
-
-
-
+    while True:
+        GPIO.output(37, GPIO.LOW)
+        time.sleep(1)
+        print("turn on")
+        GPIO.output(37, GPIO.HIGH)
+        print("turn off")
+        time.sleep(1)
 
 
 if __name__ == '__main__':
-    pass
+    n = input("model:")
+    while n == 1 or n == 2 or n == 3:
+        if n == 1:
+            LED_shine()
+        elif n == 2:
+            LED_turn_on()
+        elif n == 3:
+            LED_turn_off()
